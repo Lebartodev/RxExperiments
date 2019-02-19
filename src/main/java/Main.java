96f1b7f0d483c1d5995f831c540964daeddf2e9e
Vorobyov Denis <vorobyov@mercurydevelopment.com>
2019-02-19 15:59:05 +0400

    Non stop timer implementation.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,54 +1,35 @@
-import com.jakewharton.rxrelay2.BehaviorRelay;
-import com.jakewharton.rxrelay2.Relay;
-import io.reactivex.Observable;
-import io.reactivex.Single;
-import io.reactivex.schedulers.Schedulers;
-import io.reactivex.subjects.BehaviorSubject;
-
-import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
+import com.jakewharton.rxrelay2.BehaviorRelay;
+
+import io.reactivex.Observable;
+import io.reactivex.schedulers.Schedulers;
+
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
-//        Observable<Integer> a = Observable.merge(Observable.fromArray(1, 2, 3, 4), Observable.fromArray(10, 11, 12, 13)).map(h -> {
-//            if (h == 3) {
-//                throw new NullPointerException();
-//            } else return h;
-//        }).onErrorReturn(th -> 3);
-//
-//
-//        BehaviorRelay<Integer> behaviorRelay = BehaviorRelay.create();
-//
-//        behaviorRelay.subscribe(System.out::println);
-//        a.subscribe(behaviorRelay, throwable -> {
-//
-//        });
+	public static void main(String[] args) {
+		BehaviorRelay<Long> test = BehaviorRelay.create();
+		test.subscribe(System.out::println, System.out::println, () -> System.out.println("complete"));
 
-        BehaviorRelay<Long> test = BehaviorRelay.create();
-        test.subscribe(System.out::println, System.out::println, () -> System.out.println("cin"));
+		Observable.interval(10, 200, TimeUnit.MILLISECONDS, Schedulers.io())
+				.flatMap(intervalValue -> {
+					Observable<Object> objectObservable = Observable.create(emitter -> {
+						if (intervalValue % 10 == 3) {
+							throw new Exception();
+						}
+						emitter.onNext(intervalValue);
+					});
 
-        Observable.interval(10, 10, TimeUnit.MILLISECONDS, Schedulers.io())
-                .flatMap(intervalValue -> {
-                    return Observable.defer(() -> {
-                        Random random = new Random();
-                        int i = random.nextInt(200);
-                        if (i > 150) {
-                            throw new NullPointerException();
-                        }
-                        return Observable.just(intervalValue);
-                    });
-                }).subscribe(test, System.out::println);
-        Thread.sleep(5000);
-        test.accept(1L);
-        test.accept(1L);
-        test.accept(2L);
-        test.accept(4L);
-        test.accept(3L);
-        test.accept(1L);
-        test.accept(2L);
+					objectObservable.onErrorResumeNext(objectObservable)
+							.subscribe(value -> {
+							}, System.out::println);
+					return Observable.just(intervalValue);
+				})
+				.subscribe(test, System.out::println);
 
-        Thread.sleep(5000);
-    }
-
-
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 }
